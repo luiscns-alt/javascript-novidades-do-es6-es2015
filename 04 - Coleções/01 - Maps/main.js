f4b6af6077eb11fe3c0d248ceea3e8c55c8ac1b9
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-27 22:07:08 -0300

    fix: maps

--- 04 - Coleções/01 - Maps/main.js
+++ 04 - Coleções/01 - Maps/main.js
@@ -14,3 +14,7 @@ var a = { value: 1 },
 myObj.set(a, 5);
 myObj.set(b, 10);
 console.log(myObj.get(a), myObj.get(b));
+
+// Não podemos usar “[ ]” para acessar as propriedades. Para isso, use “get()” e “set()”;
+// Podemos limpar todo o conteúdo com a função “clear()”;
+// Para pegar o número de chaves, use “size” ao invés de “length”.
