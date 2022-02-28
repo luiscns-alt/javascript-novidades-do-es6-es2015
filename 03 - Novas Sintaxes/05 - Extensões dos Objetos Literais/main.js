e816b5e167cc425dd154b87125088485e1cfbf14
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-27 21:53:29 -0300

    funcionalidades novas nos objetos literais

---
+++ 03 - Novas Sintaxes/05 - Extensões dos Objetos Literais/main.js
@@ -0,0 +1,27 @@
+var a = 5,
+    b = 7;
+
+var obj = {
+    a: a,
+    b: b,
+};
+
+var a = 5,
+    b = 7;
+
+var obj = {
+    a: a,
+    b: b,
+};
+
+var obj = {
+    x: function () {
+        console.log(this);
+    },
+};
+
+var obj = {
+    x() {
+        console.log(this);
+    },
+};
--- Projeto/scripts/app.js
+++ Projeto/scripts/app.js
@@ -1,24 +1,12 @@
-var myList = [1, 10, 15, 5, 2, 27, 31];
+var a = 5,
+    b = 7;
 
-myList.sort((a, b) => (a > b ? 1 : -1));
-
-console.log(myList);
-
-var myList2 = [{ a: 1 }, { a: 2 }, { a: 3 }];
-
-var myNewList = myList2.map((obj) => obj.a);
-
-console.log(myNewList);
-
-var myObj = {
-    a: 5,
-    myFunc: function () {
-        console.log(this.a);
-        var newFunc = () => {
-            console.log(this.a);
-        };
-        newFunc();
+var obj = {
+    a,
+    b,
+    x() {
+        console.log(this);
     },
 };
 
-myObj.myFunc();
+obj.x();
