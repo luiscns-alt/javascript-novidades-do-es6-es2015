d2b1a7a53a05e4526cb835b191cba2c6a0219f5f
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-27 21:40:52 -0300

    template literals

---
+++ 03 - Novas Sintaxes/02 - Template Literals/main.js
@@ -0,0 +1,7 @@
+var myVar = 5;
+console.log(`My Var value is ${myVar}`);
+
+var myString = `a
+b
+c
+`;
--- Projeto/scripts/app.js
+++ Projeto/scripts/app.js
@@ -1,7 +1,3 @@
-var myList = [3, 4, 5];
+var myVar = 5;
 
-function myFunc(a, b, ...c) {
-    console.log(a, b, c);
-}
-
-myFunc(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(`My var value is: ${myVar}`);
